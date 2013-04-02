c675c26b9eece8e030c750ab57fc3c8fd8c04437
matthewcarver <carver520@gmail.com>
2013-04-02 17:51:34 -0400

    adding lesson

---
+++ assets/js/application.js
@@ -0,0 +1,16 @@
+(function($){
+	
+// TABLE OF CONTENTS
+//
+// 1 - Initial AJAX request
+// 2 - 
+// 3 - 
+
+
+
+$(function() {
+ 
+
+});
+
+})( jQuery.noConflict() ); // Pass in jQuery so we can safely use the $ alias within this block
